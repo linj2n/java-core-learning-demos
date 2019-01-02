75566591679f2f2db557138c434586a98d1b9abf
linj2n <linj2n@163.com>
2019-01-02 22:07:59 +0800

    🚧 Update
    NONE #31

--- src/test/java/cn/linj2n/javacore/collection/MapInterfaceTest.java
+++ src/test/java/cn/linj2n/javacore/collection/MapInterfaceTest.java
@@ -9,7 +9,11 @@ public class MapInterfaceTest {
 
     private List<String> words;
 
-    private Map<String, Integer> map;
+    private Map<String, Integer> hashMap = new HashMap<>();
+
+    private Map<String, Integer> treeMap = new TreeMap<>();
+
+    private Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
 
     @Before
     public void setUp() throws Exception {
@@ -17,12 +21,21 @@ public class MapInterfaceTest {
     }
 
     @Test
-    public void putAndGet() {
-        map = new HashMap<>();
+    public void threeDifferentImplementations_put() {
+        counting(words, hashMap);
+        counting(words, treeMap);
+        counting(words, linkedHashMap);
+
+        System.out.println("hash map: " + hashMap);                 // countsByString: {delegate=1, be=1, me=1, is=2, it=2, to=3, up=1, if=1}
+        System.out.println("tree map: " + treeMap);                 // tree map: {be=1, delegate=1, if=1, is=2, it=2, me=1, to=3, up=1}
+        System.out.println("linked hash map: " + linkedHashMap);    // linked hash map: {if=1, it=2, is=2, to=3, be=1, up=1, me=1, delegate=1}
+        String str = "0P";
+        System.out.println("---> " + Character.isLetter(str.charAt(0)));
+    }
+    private void counting(List<String> words, Map<String, Integer> map) {
         words.forEach(word -> {
             Integer freq = map.get(word);
             map.put(word, (freq == null) ? 1 : freq + 1);
         });
-        System.out.print("countsByString: " + map);     //  countsByString: {delegate=1, be=1, me=1, is=2, it=2, to=3, up=1, if=1}
     }
 }
